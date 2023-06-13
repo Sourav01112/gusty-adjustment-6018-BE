cd456bd5dde4c3b9e059389647a473cc84dc7e3c
Sourav01112 <chaudharysourav997@gmail.com>
2023-06-13 20:25:11 +0530

    basic structure of backend

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const { connection } = require("./db");
+const { userRouter } = require("./routes/user.routes");
+require("dotenv").config();
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/user", userRouter);
+
+app.listen(PORT, async () => {
+  try {
+    await connection;
+    console.log("Connected to db");
+  } catch (error) {
+    console.log(error);
+  }
+
+  console.log("Server is started on port number", port);
+});
